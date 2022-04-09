90b6c955cf8df00872db98e6cde2af23f094dfac
MadhuSlokaPj <MadhuSlokaPj@MadhuSlokaPj.lan>
2022-04-09 11:39:24 -0400

    cleaned up data part 2

---
+++ create_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE youtube(
+  channel_name VARCHAR(60) NOT NULL,
+  category VARCHAR(30) NOT NULL,
+  subscribers FLOAT,
+	audience_country VARCHAR(30), avg_views FLOAT,avg_likes FLOAT, avg_comments FLOAT);
+	
+	
+
+CREATE TABLE instagram(
+  Influencer_insta_name VARCHAR(40) NOT NULL,
+  category_1 VARCHAR(30) NOT NULL,
+  followers FLOAT,
+	audience_country VARCHAR(30), authentic_engagement FLOAT,engagement_avg FLOAT);
+	
+
